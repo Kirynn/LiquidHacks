370edf994114a7a39d4a63c3d819bc8b964ffc44
Kirynn <kieran1200@gmail.com>
2020-11-06 21:59:00 -0500

    UI Runs on a seperate thread

--- LiquidHacks/Program.cs
+++ LiquidHacks/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Logging.Serilog;
@@ -8,13 +9,20 @@ namespace LiquidHacks {
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+        public static void Main(string[] args) {
+
+            Thread UI = new Thread(new ParameterizedThreadStart(StartUIThread));
+            UI.Start(args);
+        }
 
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
             => AppBuilder.Configure<App>()
                 .UsePlatformDetect()
                 .LogToDebug();
+
+        public static void StartUIThread(object sender) {
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime((string[]) sender);
+        }
     }
 }
